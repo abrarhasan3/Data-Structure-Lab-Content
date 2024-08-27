43651af0c14ae52a31c0e063d76d26040b3d7d9a
Abrar Hasan <abrarhasan1998@gmail.com>
2024-08-27 23:18:34 +0600

    Add files via upload

---
+++ Lecture 1/Lecture.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define lld long long int
+using namespace std;
+void start()
+{
+        #ifndef ONLINE_JUDGE
+ 
+        freopen("input.txt", "r", stdin);
+ 
+        freopen("output.txt", "w", stdout);
+        #endif // ONLINE_JUDGE
+ 
+}
+ 
+int main()
+{
+        start();
+
+        int n=6,i;
+
+        int a[n] = {1,2,3,4,5};
+
+        // int index, value; 
+
+        // cin>>index>>value;
+
+        // for(i =4; i>=index; i-- )
+        // {
+        //         a[i+1] = a[i];
+        // }
+        // a[index] = value;
+
+        // for(i =0; i<6; i++ )
+        // {
+        //        cout<<a[i]<<endl;
+        // }
+
+        // int delIndex ;
+
+        // cin>>delIndex;
+
+        // for(i=delIndex;i<n;i++)
+        // {
+        //         a[i] = a[i+1];
+        // }
+
+        // for(i =0; i<4; i++ )
+        // {
+        //        cout<<a[i]<<endl;
+        // }
+
+        
+        
+}
+
